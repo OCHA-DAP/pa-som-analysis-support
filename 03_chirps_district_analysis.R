d4847c09f585b82bf9dcf8c46f1013a57bff61a9
paulinenimo <paunimo@gmail.com>
2022-11-08 13:20:58 +0300

    Update 03_chirps_district_analysis.R

--- 03_chirps_district_analysis.R
+++ 03_chirps_district_analysis.R
@@ -18,6 +18,153 @@ somSF <- st_read(paste0(Sys.getenv("AA_DATA_DIR"), "/public/raw/som/cod_ab/som_c
                  layer = "Som_Admbnda_Adm2_UNDP")
 somalia_adm2_chirps_intersection <- read_csv("C:/Users/pauni/Desktop/Work/OCHA/SOM/data/somalia_adm2_chirps_intersection.csv")
 
+#####
+# setting values
+start_yr <- 2000; end_yr <- 2022
+years_interest <- seq(start_yr, end_yr, 1)
+mam_months <- c("03", "04", "05")
+ond_months <- c("10", "11", "12")
+
+sel_files <- as.vector(outer(years_interest, c(mam_months, ond_months), paste, sep="_"))
+som_files <- file_list[grepl(paste(sel_files, collapse = "|"), file_list)]
+
+read_files <- function(filePath, fileName){
+  ncObj <- nc_open(paste0(filePath, fileName))
+  DataCube <- ncvar_get(ncObj, "precipitation") %>%
+    data.frame() %>%
+    bind_cols(paste0(round(ncvar_get(ncObj, "X") + 0.025, 3), 
+                     ifelse(round(ncvar_get(ncObj, "X") + 0.025, 3) > 0, "E", "W"))) %>%
+    setNames(c(paste0(round(ncvar_get(ncObj, "Y"), 3), 
+                      ifelse(round(ncvar_get(ncObj, "Y"), 3) > 0, "N", "S")), 
+               "Longitude")) %>%
+    gather(key = "Latitude", value = "precipitation", -Longitude) %>%
+    mutate(Date = str_extract(ncObj$filename,"(?<=monthly_).+(?=_r0.05)"))
+  nc_close(ncObj)
+  return(DataCube)
+}
+
+chirpsData <- som_files %>%
+  map_df(~ read_files(file_path, .)) 
+
+#####
+# aggregating data
+aggData <- chirpsData %>%
+  mutate(Year = str_sub(Date, 1, 4), 
+         Month = str_sub(Date, -2), 
+         Season_Yr = if_else(Month %in% mam_months, "MAM", 
+                             if_else(Month %in% ond_months, "OND","")), 
+         Season = paste0(Season_Yr, Year)) %>%
+  group_by(Date, Year, Season, Season_Yr, Month) %>%
+  summarise(month_ave = mean(precipitation, na.rm = T)) %>%
+  group_by(Season, Year, Season_Yr) %>%
+  summarise(total = sum(month_ave, na.rm = T))
+
+#####
+# plotting 
+ggplot(aggData, aes(x=Year, y=total, fill=Season_Yr)) + 
+  geom_bar(stat = "identity", position = position_dodge(preserve = "single")) + 
+  ggtitle("Average MAM/OND Seasonal Rainfall for a grid across the country") + 
+  ylab("Seasonal Rainfall (mm)")
+
+
+plot(somSF$geometry)
+
+#####
+somalia_adm2_chirps_intersection <- somalia_adm2_chirps_intersection %>% 
+  mutate(GridPerc = GridArea / DistArea, 
+         Centroid = paste0(round(Longitude, 3), "_", round(Latitude,3)))
+
+chirpsData2 <- chirpsData %>%
+  mutate(Centroid = paste0(str_sub(Longitude, 1, -2), "_", str_sub(Latitude, 1, -2)))
+
+#####
+chirps_adm2 <- somalia_adm2_chirps_intersection %>%
+  left_join(chirpsData2, by = "Centroid") %>%
+  mutate(PercPrec = precipitation * GridPerc) %>%
+  group_by(admin2Pcod, Date) %>%
+  summarise(adm2prec = sum(PercPrec, na.rm = T)) %>%
+  mutate(Year = str_sub(Date, 1, 4), 
+         Month = str_sub(Date, -2), 
+         Season_Yr = if_else(Month %in% mam_months, "MAM", 
+                             if_else(Month %in% ond_months, "OND","")), 
+         Season = paste0(Season_Yr, Year)) %>%
+  group_by(Season, Year, Season_Yr, admin2Pcod) %>%
+  summarise(ssn_total = sum(adm2prec, na.rm = T)) %>%
+  left_join(somSF, by = "admin2Pcod")
+
+#####
+chirps_adm2 %>%
+  group_by(Season, Year, Season_Yr) %>%
+  summarise(dist_mean = mean(ssn_total, na.rm = T)) %>% 
+  ggplot(aes(x=Year, y=dist_mean, fill=Season_Yr)) + 
+  geom_bar(stat = "identity", position = position_dodge(preserve = "single")) + 
+  ggtitle("Average MAM/OND Seasonal Rainfall for a district across the country") + 
+  ylab("Seasonal Rainfall (mm)") 
+#####
+chirps_adm2 %>%
+  group_by(Season, Year, Season_Yr) %>%
+  summarise(dist_mean = mean(ssn_total, na.rm = T)) %>% 
+  ggplot(aes(x=Year, y=dist_mean, fill = Season_Yr)) + 
+  geom_bar(stat = "identity") + 
+  ggtitle("Average Seasonal Rainfall for a district across the country") + 
+  ylab("Seasonal Rainfall (mm)") + 
+  facet_wrap(~Season_Yr)
+
+
+#####
+ggplot(data = chirps_adm2, aes(geometry = geometry)) + 
+  geom_sf(aes(fill = ssn_total)) + 
+  ggtitle("Seasonal Rainfall") + 
+  labs(fill = "Rainfall (mm)") + 
+  xlab("Longitude") + 
+  ylab("Latitude") + 
+  theme(text = element_text(size = 8), legend.key.size = unit(0.5, 'cm')) + 
+  scale_fill_stepsn(colours = c("#ff0000", "#ff7f00", "#ffffff", "#64f964", "#1d7407"),
+                    breaks = c(0, 100, 140, 180, 220, 400),
+                    limit = c(0, 400),
+                    na.value = "#BEBEBE") + 
+  theme(axis.text.x = element_text(angle = 90)) + 
+  facet_wrap(~Season, ncol = 23)
+
+#####
+consec_seasons <- c("MAM2020", "OND2020", "MAM2021", "OND2021", "MAM2022")
+
+SeasonData <- chirps_adm2 %>%
+  filter(Season %in% consec_seasons)
+
+ggplot(data = SeasonData, aes(geometry = geometry)) + 
+  geom_sf(aes(fill = ssn_total)) + 
+  ggtitle("Seasonal Rainfall") + 
+  labs(fill = "Rainfall (mm)") + 
+  xlab("Longitude") + 
+  ylab("Latitude") + 
+  theme(text = element_text(size = 15), legend.key.size = unit(0.5, 'cm')) + 
+  scale_fill_stepsn(colours = c("#ff0000", "#ff7f00", "#ffffff", "#64f964", "#1d7407"),
+                    breaks = c(0, 100, 140, 180, 220, 400),
+                    limit = c(0, 400),
+                    na.value = "#BEBEBE") + 
+  facet_wrap(~Season)
+
+#####
+sel_seasons <- c("MAM2011", "MAM2017", "MAM2022", "OND2011", "OND2017")
+
+SeasonData <- chirps_adm2 %>%
+  filter(Season %in% sel_seasons)
+
+ggplot(data = SeasonData, aes(geometry = geometry)) + 
+  geom_sf(aes(fill = ssn_total)) + 
+  ggtitle("Seasonal Rainfall") + 
+  labs(fill = "Rainfall (mm)") + 
+  xlab("Longitude") + 
+  ylab("Latitude") + 
+  theme(text = element_text(size = 15), legend.key.size = unit(0.5, 'cm')) + 
+  scale_fill_stepsn(colours = c("#ff0000", "#ff7f00", "#ffffff", "#64f964", "#1d7407"),
+                    breaks = c(0, 100, 140, 180, 220, 400),
+                    limit = c(0, 400),
+                    na.value = "#BEBEBE") + 
+  facet_wrap(~Season)
+
+#####
 
 
 
